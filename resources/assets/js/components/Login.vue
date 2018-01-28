730ee6911e38a2a9954041d5b24fd1f13ce61d3b
Alex Christian <aquispe.developer@gmail.com>
2018-01-28 16:20:29 -0500

    release

---
+++ resources/assets/js/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+<script>
+    /**
+     * Created by aquispe on 28/01/2018.
+     **/
+    export default {
+        data: () => ({}),
+        beforeCreate() {
+        },
+        created() {
+        },
+        computed: {},
+        methods: {}
+    }
+</script>  
